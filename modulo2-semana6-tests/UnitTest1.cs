75d5ade05be838a62c830a0c3c0327e94ce9f2cd
FridayAI <gabi.get@hotmail.com>
2022-07-07 19:39:08 -0300

    Add unit test failure for API

--- modulo2-semana6-tests/UnitTest1.cs
+++ modulo2-semana6-tests/UnitTest1.cs
@@ -13,4 +13,16 @@ public class VerdadeiroFalsoTest : ConfiguracaoHostApi
         Assert.NotNull(responseApi);
         Assert.Equal("verdadeiro", responseApi);
     }
+
+    [Theory]
+    [InlineData("Peixe")]
+    public async Task Test_Api_True_False_Failure(string type)
+    {
+        var result = await client.GetAsync($"/ExercicioVerdadeiroFalso/{type}");
+        Assert.NotNull(result);
+
+        var responseApi = await result.Content.ReadAsStringAsync();
+        Assert.NotNull(responseApi);
+        Assert.Equal("verdadeiro", responseApi);
+    }
 }

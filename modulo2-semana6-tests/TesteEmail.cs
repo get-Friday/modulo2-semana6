13a1e637603cdbc47be2e2a18ca955ffbef6745a
FridayAI <gabi.get@hotmail.com>
2022-07-08 15:10:11 -0300

    Add test api email request success

---
+++ modulo2-semana6-tests/TesteEmail.cs
@@ -0,0 +1,17 @@
+﻿namespace modulo2_semana6_tests;
+
+public class TesteEmail : ConfiguracaoHostApi
+{
+    [Theory]
+    [InlineData("email@email.com")]
+    public async Task Test_Api_Email_Request_Success(string email)
+    {
+        var result = await client.GetAsync($"/ExercicioEmail/{email}");
+        Assert.NotNull(result);
+
+        var responseApi = await result.Content.ReadAsStringAsync();
+        Assert.NotNull(responseApi);
+        Assert.Equal(email, responseApi);
+    }
+}
+

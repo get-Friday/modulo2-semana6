d818f53aed10ea9a7ffd274d56947571772ccb2c
FridayAI <gabi.get@hotmail.com>
2022-07-08 15:47:01 -0300

    Add sum success test

---
+++ modulo2-semana6-tests/TesteSoma.cs
@@ -0,0 +1,17 @@
+﻿namespace modulo2_semana6_tests;
+
+public class SomaTest : ConfiguracaoHostApi
+{
+    [Theory]
+    [InlineData(5, 3)]
+    public async Task Test_Sum_Success(int x, int y)
+    {
+        var result = await client.GetAsync($"/ExercicioSoma/{x}/{y}");
+        Assert.NotNull(result);
+
+        var responseApi = await result.Content.ReadAsStringAsync();
+        Assert.NotNull(responseApi);
+        Assert.Equal($"{x} + {y} = {x + y}", responseApi);
+    }
+}
+
